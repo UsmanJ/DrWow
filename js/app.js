a4f79dba5ecc4656ecc3b38419ad835f89348b9b
Usman Jamil <usman.jamil@live.co.uk>
2015-11-03 11:46:03 +0000

    front end set up with express server

---
+++ js/app.js
@@ -0,0 +1,1 @@
+var drWow = angular.module('DrWow', ['ngResource']);
---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+// app.use(express.static(__dirname + 'www/'+ ''));
+
+
+app.get('/', function(req, res) {
+    res.sendfile('./index.html');
+});
+
+app.listen(port, function() {
+  console.log('Our app is running on http://localhost:' + port);
+});

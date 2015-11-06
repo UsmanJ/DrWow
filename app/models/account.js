b63277a40475f379eee019d80a5c5e2ba8bc3022
philip <philip.clarke@redhood.co.uk>
2015-11-06 13:44:16 +0000

    auth working and refactored

--- app/models/account.js
+++ app/models/account.js
@@ -1,17 +1,4 @@
-// // app/models/user.js
-//
-// var mongoose     = require('mongoose');
-// var Schema       = mongoose.Schema;
-// var passportLocalMongoose = require('passport-local-mongoose')
-//
-// var UserSchema   = new Schema({
-//     name: String,
-//     password: String
-// });
-//
-// UserSchema.plugin(passportLocalMongoose);
-//
-// module.exports = mongoose.model('user', UserSchema);
+
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
--- server.js
+++ server.js
@@ -97,93 +97,6 @@ var Consultation = require('./app/models/consultation');
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
-// // middleware to use for all requests
-// router.use(function(req, res, next) {
-//     // do logging
-//     console.log('request was sent to our API');
-//     next(); // make sure we go to the next routes and don't stop here
-// });
-//
-//
-// // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-// router.get('/', function(req, res) {
-//     res.json({ message: 'hooray! welcome to our api!' });
-// });
-
-// more routes for our API will happen here
-//
-// router.route('/users')
-//
-//   // create a user (accessed at POST http://localhost:8080/api/users)
-//   .post(function(req, res) {
-//
-//       var user = new User();   // create a new instance of the User model
-//       user.name = req.body.name;  // set the users name (comes from the request)
-//
-//       console.log(user);
-//       console.log(db.connection.readyState);
-//
-//       // save the user and check for errors
-//       user.save(function(err) {
-//           if (err)
-//             res.send(err);
-//
-//           res.json({ message: 'User created!' });
-//       });
-//   })    //adding a semi-colon here raises error
-//
-//   // get all the users (accessed at GET http://localhost:8080/api/users)
-//   .get(function(req, res) {
-//     User.find(function(err, users) {
-//         if (err)
-//           res.send(err);
-//
-//         res.json(users);
-//     });
-//   });
-//
-// router.route('/users/:user_id')
-//
-//   // get the user with that id (accessed at GET http://localhost:8080/api/users/:user_id)
-//   .get(function(req, res) {
-//     User.findById(req.params.user_id, function(err, user) {
-//       if (err)
-//         res.send(err);
-//       res.json(user);
-//     });
-//   })
-//
-//   // update the user with this id (accessed at PUT http://localhost:8080/api/users/:user_id)
-//   .put(function(req, res) {
-//
-//     // use our user model to find the user we want
-//     User.findById(req.params.user_id, function(err, user) {
-//       if (err)
-//         res.send(err);
-//       user.name = req.body.name;  // update the users info
-//
-//     // save the user
-//       user.save(function(err) {
-//       if (err)
-//          res.send(err);
-//
-//       res.json({ message: 'User updated!' });
-//       });
-//     });
-//   })
-//
-//   // delete the user with this id (accessed at DELETE http://localhost:8080/api/users/:user_id)
-//   .delete(function(req, res) {
-//
-//     User.remove({
-//       _id: req.params.user_id
-//     }, function(err, user) {
-//       if (err)
-//         res.send(err);
-//
-//       res.json({ message: 'Successfully deleted' });
-//     });
-//   });
 //
  router.route('/consultations')
 
@@ -261,16 +174,6 @@ router.route('/consultations/:consultation_id')
     });
   });
 
-
-
-
-// REGISTER OUR ROUTES -------------------------------
-// // all of our routes will be prefixed with /api
-// app.use('/api', router);
-
-// app.get('/', function(req, res) {
-//     res.sendfile('index.html');
-// });
 
 app.listen(port, function() {
   console.log('Our app is running on http://localhost:' + port);

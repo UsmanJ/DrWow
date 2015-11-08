476b80e7f01a7045cecf5ac0d7c7843c70d332ac
Usman Jamil <usman.jamil@live.co.uk>
2015-11-08 00:00:23 +0000

    two db and two log in portals

--- app/models/patient.js
+++ app/models/patient.js
@@ -4,9 +4,25 @@ var passportLocalMongoose = require('passport-local-mongoose');
 
 var Patient = new Schema({
     username: String,
-    password: String
+    password: String,
+    name: String,
+    email: String,
+    gender: String,
+    age: String,
+    address: String
 });
 
 Patient.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('patients', Patient);
+
+// .form-group
+//     input.form-control(type='text', name="fullname", placeholder='Enter Name')
+// .form-group
+//     input.form-control(type='text', name="email", placeholder='Enter Email')
+// .form-group
+//     input.form-control(type='text', name="gender", placeholder='Enter Gender')
+// .form-group
+//     input.form-control(type='text', name="age", placeholder='Enter Age')
+// .form-group
+//     input.form-control(type='text', name="address", placeholder='Enter Address')

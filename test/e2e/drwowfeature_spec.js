1b56aacde8ab7f0d2c8691c70ac1f1cb532f59e4
philip <philip.clarke@redhood.co.uk>
2015-11-09 19:07:13 +0000

    login test passing

--- test/e2e/drwowfeature_spec.js
+++ test/e2e/drwowfeature_spec.js
@@ -9,12 +9,18 @@ describe('DrWoW login', function() {
     expect(browser.driver.getTitle()).toEqual('DrWow');
   });
 
+  it('you can register as a user', function(){
+      browser.driver.get('http://localhost:8080/register');
+      
+  });
+
   it('You can login', function() {
+      browser.driver.get('http://localhost:8080/login');
       browser.driver.findElement(by.id('username')).sendKeys('111test@test.co.uk');
       browser.driver.findElement(by.id('password')).sendKeys('123makers');
       browser.driver.findElement(by.id('submit')).click();
 
-      expect(element(by.css('h1')).getText()).toBe('Dr WoW');
+      expect(element(by.css('h1')).getText()).toBe('You are currently logged in as 111test@test.co.uk');
   });
 
 
